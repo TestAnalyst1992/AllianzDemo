65ea8f851be55288cac2f9acd384d97e9b3d500c
Sinju Zacharias <sinjuzacharias1992@gmail.com>
2023-08-13 20:55:25 +0530

    My First Commit

---
+++ src/main/java/sinju/quadance/co/in/GitHubBasics/App.java
@@ -0,0 +1,13 @@
+package sinju.quadance.co.in.GitHubBasics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
